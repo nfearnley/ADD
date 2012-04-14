f154e6dc0238ad5e763ee34e3395af5a1c971797
Myles McKay <myles.mckay@gmail.com>
2012-04-14 14:34:54 -0400

    Added values for Client

--- ADD Demo/Classes/Client.cs
+++ ADD Demo/Classes/Client.cs
@@ -7,5 +7,23 @@ namespace ADD_Demo.Classes
 {
     public class Client
     {
+        private int clientID;
+        private String WorkPhone;
+        private String LastName;
+        private String HomePhone;
+        private String FirstName;
+        private String FaxPhone;
+        private String AddressRegion;
+        private String AddressPostalCode;
+        private String AddressLine1;
+        private String AddressLine2;
+        private String AddressCountry;
+        private String AddressCity;
+        private int CompanyID;
+
+        public Client()
+        { }
+
+
     }
 }
