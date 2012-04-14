75431983bd01ed28385bc7046370804827cef008
Myles McKay <myles.mckay@gmail.com>
2012-04-14 14:51:43 -0400

    Added Instructor Attributes

--- ADD Demo/Classes/Instructor.cs
+++ ADD Demo/Classes/Instructor.cs
@@ -7,5 +7,16 @@ namespace ADD_Demo.Classes
 {
     public class Instructor
     {
+        private int InstructorID;
+        private String AddressCity;
+        private String AddressCountry;
+        private String AddressLine1;
+        private String AddressLine2;
+        private String AddressPostalCode;
+        private String AddressRegion;
+        private String AltPhone;
+        private String FirstName;
+        private String HomePhone;
+        private String LastName;
     }
 }
