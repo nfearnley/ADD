65b537c57790d25559ab36c426aac33f2dc7a64e
Myles McKay <myles.mckay@gmail.com>
2012-04-14 14:40:54 -0400

    Added Company Attributes

--- ADD Demo/Classes/Company.cs
+++ ADD Demo/Classes/Company.cs
@@ -7,5 +7,12 @@ namespace ADD_Demo.Classes
 {
     public class Company
     {
+        private int CompanyID;
+        private String BillingAddressCity;
+        private String BillingAddressCountry;
+        private String BillingAddressLine1;
+        private String BillingAddressPostalCode;
+        private String BillingAddressRegion;
+        private String BillingName;
     }
 }
