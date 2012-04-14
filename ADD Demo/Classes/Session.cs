a37d5b647548a56a94d0f319f2a10cdd4f31e06d
Myles McKay <myles.mckay@gmail.com>
2012-04-14 15:10:32 -0400

    Added Session Attributes

--- ADD Demo/Classes/Session.cs
+++ ADD Demo/Classes/Session.cs
@@ -7,5 +7,10 @@ namespace ADD_Demo.Classes
 {
     public class Session
     {
+        private int SessionID;
+        private int InstructorID;
+        private int RoomID;
+        private String Length;
+        private DateTime datetime;
     }
 }
