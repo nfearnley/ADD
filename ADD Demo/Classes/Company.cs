6b7575547434fba19339a8de00d547988c56e23b
Myles McKay <myles.mckay@gmail.com>
2012-04-15 14:15:33 -0400

    layout of functions for Company and Instructor Classes

--- ADD Demo/Classes/Company.cs
+++ ADD Demo/Classes/Company.cs
@@ -11,8 +11,49 @@ namespace ADD_Demo.Classes
         private String BillingAddressCity;
         private String BillingAddressCountry;
         private String BillingAddressLine1;
+        private String BillingAddressLine2;
         private String BillingAddressPostalCode;
         private String BillingAddressRegion;
         private String BillingName;
+
+        public Company(String city, String country, String line1, String line2, String code, String region, String name)
+        {
+            BillingAddressCity = city;
+            BillingAddressCountry = country;
+            BillingAddressLine1 = line1;
+            BillingAddressLine2 = line2;
+            BillingAddressPostalCode = code;
+            BillingAddressRegion = region;
+            BillingName = name;
+        }
+
+        public    Company()
+        {
+        
+        }
+
+        public static Company GetCompany(int companyID)
+        {
+            return new Company();
+        }
+
+        public static List<Company> GetCompanies(int companyID)
+        {
+            return new List<Company>();
+
+        }
+
+        public static void AddCompany(String city, String country, String line1, String line2, String code, String region, String name)
+        {}
+
+        public static Company RemoveCompany(int companyID)
+        {
+            return new Company();
+        }
+
+        public static bool UpdateCompany(String city, String country, String line1, String line2, String code, String region, String name)
+        {
+            return false;
+        }
     }
 }
--- ADD Demo/Classes/Instructor.cs
+++ ADD Demo/Classes/Instructor.cs
@@ -18,5 +18,47 @@ namespace ADD_Demo.Classes
         private String FirstName;
         private String HomePhone;
         private String LastName;
+
+        public Instructor()
+        { }
+
+        public Instructor(String city, String country, String line1,String line2, String code, String region,String altPhone,String fName,String lName,String hPhone)
+        { 
+            AddressCity = city;
+            AddressCountry = country;
+            AddressLine1 = line1;
+            AddressLine2 = line2;
+            AddressPostalCode = code;
+            AddressRegion = region;
+            AltPhone = altPhone;
+            FirstName = fName;
+            HomePhone = hPhone;
+            LastName = lName;
+        }
+
+        public static Instructor GetInstructor(int instructorID)
+        { 
+            return new Instructor;
+        }
+
+        public static List<Instructor> GetInstructors()
+        {
+            return new List<Instructor>();
+        }
+
+        public static void AddInstructor(Instructor inst)
+        { 
+        
+        }
+
+        public static Instructor RemoveInstructor(int instructorID)
+        {
+            return new Instructor();
+        }
+
+        public static bool UpdateInstructor(Instructor inst)
+        {
+            return false;        
+        }
     }
 }
