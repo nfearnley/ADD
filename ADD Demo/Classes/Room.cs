8dfdcef8f73625fe05fbd5a0ec2f16985cfe40eb
Myles McKay <myles.mckay@gmail.com>
2012-04-15 16:41:51 -0400

    Function Layout for
    	Room
    	Session
    	Status
    This has no implementation just default values.

--- ADD Demo/Classes/Room.cs
+++ ADD Demo/Classes/Room.cs
@@ -10,5 +10,39 @@ namespace ADD_Demo.Classes
         private int RoomID;
         private int Seats;
         private String RoomName;
+
+        public Room()
+        { }
+
+        public Room(int seats, String roomName)
+        {
+            Seats = seats;
+            RoomName = roomName;
+        }
+
+        public static Room GetRoom(int roomID)
+        {
+            return new Room();
+        }
+
+        public static List<Room> GetRooms()
+        {
+            return new List<Room>();
+        }
+
+        public static void AddRoom(Room room)
+        { 
+            
+        }
+
+        public static bool RemoveRoom(int roomID)
+        {
+            return false;
+        }
+
+        public static bool UpdateRoom(Room room)
+        {
+            return false;
+        }
     }
 }
